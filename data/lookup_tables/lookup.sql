c8e7be95f978370c4d3465f4d55d824622739afd
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-08-14 11:36:25 +0530

    Update lookup.sql

--- data/lookup_tables/lookup.sql
+++ data/lookup_tables/lookup.sql
@@ -1,12 +1,1 @@
---liquibase formatted sql
---changeset AUTHOR:CHANGESET_NAME runOnChange:true stripComments:false
---comment OPTIONAL COMMENT
-
--- Create the main table to store product information
-CREATE TABLE products (
-    product_id INT PRIMARY KEY,
-    product_name VARCHAR(100),
-    category_id INT,
-    price DECIMAL(10, 2)
-);
 
